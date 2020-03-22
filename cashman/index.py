2e4a874bbac599df99103ba083860655c07e1828
gortron <gordon.lanza@gmail.com>
2020-03-22 16:40:03 -0700

    refactored index to handle get/post methods for /incomes and /expenses routes

--- cashman/index.py
+++ cashman/index.py
@@ -1,18 +1,50 @@
 from flask import Flask, jsonify, request
 
+from cashman.models.expense import Expense, ExpenseSchema
+from cashman.models.income import Income, IncomeSchema
+from cashman.models.transaction_type import TransactionType
+
 app = Flask(__name__)
 
-incomes = [
-  { 'description': 'salary', 'amount': 5000 }
+transactions = [
+  Income('Salary', 5000),
+  Income('Dividends', 200),
+  Expense('pizza', 50),
+  Expense('Rock Concert', 100)
 ]
 
 
-@app.route('/incomes')
+@app.route('/incomes', methods=['GET'])
 def get_incomes():
+  schema = IncomeSchema(many=True)
+  incomes = schema.dump(
+    filter(lambda t: t.type == TransactionType.INCOME, transactions)
+  )
   return jsonify(incomes)
 
 
 @app.route('/incomes', methods=['POST'])
 def add_income():
-  incomes.append(request.get_json())
-  return '', 204
+  income = IncomeSchema().load(request.get_json())
+  transactions.append(income.data)
+  return "", 204
+
+
+@app.route('/expenses', methods=['GET'])
+def get_expenses():
+  schema = ExpenseSchema(many=True)
+  expenses = schema.dump(
+      filter(lambda t: t.type == TransactionType.EXPENSE, transactions)
+  )
+  return jsonify(expenses)
+
+
+@app.route('/expenses', methods=['POST'])
+def add_expense():
+  expense = ExpenseSchema().load(request.get_json())
+  transactions.append(expense.data)
+  return "", 204
+
+
+if __name__ == "__main__":
+    app.run()
